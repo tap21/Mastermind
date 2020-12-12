6c164672e2a8584ad1dd65b2bb9ad4aba48ad962
Taylor Piccorelli <piccorelli.t@gmail.com>
2020-12-11 19:39:50 -0500

    Mastermind second draft

--- MastermindApp/Mastermind.cs
+++ MastermindApp/Mastermind.cs
@@ -8,35 +8,34 @@ namespace MastermindApp
 
         public static void Main()
         {
+            Mastermind mastermind = new Mastermind();
 
             int numberOfAttempts = NumberOfAttempts;
 
+            string randomNumber = GenerateRandomNumber();
+
             while (numberOfAttempts <= 10 && numberOfAttempts > 0)
             {
-                
-               string randomNumber = GenerateRandomNumber();
                numberOfAttempts--;
                 // this line was used to test the program and compare the userInput to the generated randomNumber
                //Console.WriteLine("Your starting number is: " + randomNumber);
+
                Console.WriteLine("Please guess a 4 digit number. Note: You can only use digits between 1-6.");
                string userInput = Console.ReadLine();
-                if (userInput.Length == 4) 
-                {
-                    CheckNumber(randomNumber, userInput);
-                    Console.WriteLine("Number of attempts remaining: " + numberOfAttempts);
-                }
-                else
-                {
-                    Console.WriteLine("To play, the entered value must be 4 digits in length.");
-                }
                
+              string result = mastermind.PrintChecksAndMinuses(randomNumber, userInput);
+              Console.WriteLine("Number of attempts remaining: " + numberOfAttempts);
+
+              Console.WriteLine($"{result}");
                 
-              
-            }
-                // if userInput is entered 10 times, console spits out message saying user has 0 attempts left
-                Console.WriteLine("Sorry, you have exhausted your 10 attempts.");
             
+            }
+            
+                // if userInput is entered 10 times, game is over and randomNumber is disclosed to user
+                Console.WriteLine($"Sorry, you have exhausted your 10 attempts. Your number was {randomNumber}.");
         }
+
+
 
         // Generates a random 4 digit number
         public static string GenerateRandomNumber()
@@ -58,92 +57,173 @@ namespace MastermindApp
 
         }
 
-        public static void CheckNumber(string randomNumber, string userInput)
+
+        // Checks to make sure the userInput is 4 in length with values between 1-6
+        public bool IsUserInputValid(string userInput)
         {
-            int correctDigit = 0;
-            int correctPosition = 0;
-            bool isFound = false;
+           if(userInput.Length != 4)
+           {
+               return false;
 
-            for (int i = 0; i < userInput.Length; i++)
+           }
+            foreach (char c in userInput)
             {
-                isFound = false;
-
-                for (int j = 0; j < randomNumber.Length; j++)
+                if(!(c >= '0' && c <= '6'))
                 {
-                    
-                    if (isFound == false)
-                    {
-                        // compares the values
-                        if ((userInput[i] == randomNumber[j]) && (userInput[i] != 0))
-                        {
-                            isFound = true;
-                            // checks to see if values are in the same position
-                            if (i == j)
-                            {
-                                correctPosition++;
-
-                            }
-                            else
-                            {
-                                correctDigit++;
-
-                            }
-                           
-                        }
-                        
-                   
-                    }
-                   
-
+                    return false;
                 }
+            }
+                
+            return true;
+        }
+
+
+        // Adds all '+' scenarios
+        public string CheckForCorrectPositionCorrectValue(string randomNumber, string userInput)
+        {
+            string correctPositionCorrectValue = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (randomNumber[i] == userInput[i])
+                {
+                    correctPositionCorrectValue += "+";
+                }
+            }
+            return correctPositionCorrectValue;
+        }
+
+        // Adds all '-' scenarios
+        public string CheckForCorrectValueIncorrectPosition(string randomNumber, string userInput)
+        {
+            string correctValueIncorrectPosition = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (randomNumber.Contains(userInput[i]) && randomNumber[i] != userInput[i])
+                {
+                    correctValueIncorrectPosition += "-";
+                }
+            }
+            return correctValueIncorrectPosition;
+
+        }
+       
+        // Combines both '+' and '-' scenarios together
+        public string PrintChecksAndMinuses(string randomNumber, string userInput)
+        {
+
+            if (IsUserInputValid(userInput) == true)
+            {
+                string plusSigns = CheckForCorrectPositionCorrectValue(randomNumber, userInput);
+                string minusSigns = CheckForCorrectValueIncorrectPosition(randomNumber, userInput);
+                string combinedPlusAndMinuses = plusSigns + minusSigns;
+                return combinedPlusAndMinuses;
+            }
+            else
+            {
+                return "You have entered an incorrect value.";
+            }
+       
+        }
+
+
+
+
+        //public static void CheckNumber(string randomNumber, string userInput)
+        //{
+        //    int correctDigit = 0;
+        //    int correctPosition = 0;
+        //    bool isFound = false;
+
+        //    for (int i = 0; i < userInput.Length; i++)
+        //    {
+        //        isFound = false;
+
+        //        if (userInput[i] == randomNumber[i])
+        //        {
+        //            correctPosition++;
+        //        }
+        //        else
+        //        {
+        //            for (int j = 0; j < randomNumber.Length; j++)
+        //            {
+        //                if (userInput[i] == userInput[j])
+        //                {
+        //                    correctDigit++;
+        //                }
+        //               /* if (isFound == false)
+        //                {
+        //                    // compares the values
+
+        //                    if ((userInput[i] == randomNumber[j]) && (userInput[i] != 0))
+        //                    {
+        //                        isFound = true;
+        //                        // checks to see if values are in the same position
+        //                        if (i == j)
+        //                        {
+        //                            correctPosition++;
+
+        //                        }
+        //                        else
+        //                        {
+        //                            correctDigit++;
+
+        //                        }
+        //                    }
+        //                }*/
+        //            }
+
+        //        }
                
 
 
-            }
+        //    }
             
 
 
-            Console.WriteLine("Numbers in the correct position: " + correctPosition + " Correct digits but in wrong position: " + correctDigit);
+        //    Console.WriteLine("Numbers in the correct position: " + correctPosition + " Correct digits but in wrong position: " + correctDigit);
 
-            // string to hold '+' and '-' 
-            string result = "";
+        //    // string to hold '+' and '-' 
+        //    string result = "";
 
-            // loop through correctPosition to find '+'
-            for (int i = 0; i < correctPosition; i++)
-            {
-                result = result + "+";
-            }
+        //    // loop through correctPosition to find '+'
+        //    for (int i = 0; i < correctPosition; i++)
+        //    {
+        //        result = result + "+";
+        //    }
 
-            // loop through correctdigits to find '-'
-            for (int i = 0; i < correctDigit; i++)
-            {
-                if (correctDigit != correctPosition) 
-                {
-                    result = result + "-";
-                }
-            }
-
-
-            if (result != "")
-            {
-                Console.WriteLine(result);
-            }
+        //    // loop through correctdigits to find '-'
+        //    for (int i = 0; i < correctDigit; i++)
+        //    {
+        //        if (correctDigit != correctPosition) 
+        //        {
+        //            result = result + "-";
+        //        }
+        //    }
 
 
-            // if user wins, display the following message
+        //    if (result != "")
+        //    {
+        //        Console.WriteLine(result);
+        //    }
 
-            if (isFound)
-            {
-                if (result == "++++") 
-                {
-                    Console.WriteLine("You won!");
-                }
+
+        //    // if user wins, display the following message
+
+        //    if (isFound)
+        //    {
+        //        if (result == "++++") 
+        //        {
+        //            Console.WriteLine("You won!");
+        //        }
                 
-            }
+        //    }
 
 
 
-        }
+        //}
+
 
 
 
